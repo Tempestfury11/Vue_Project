14c1204e39b9b95295d36546778e349ec2d1dd90
Tempestfury11 <Swiftyblue11@gmail.com>
2022-07-22 09:24:12 +0200

    Contact About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,87 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="container">
+        <h2 class="text-center"><span>About Me</span></h2>
+        <h3 class="text-center"><il>My Name Is Marshalino Jankowski</il></h3>
+        <br>
+        <div class="row">
+          <div class="col-md-4">
+            <img class="img-fluid"
+              src="https://i.postimg.cc/43HGTqKM/LS2.jpg"
+              alt="aboutmeimage" defer
+            />
+          </div>
+          <div id="abouttext" class="col-md-8">
+            <p>
+              I am a young old soul who enjoys the outdoors, loves sports and plays games with a passion.
+              <br>
+              I would say that I am honest, trustworthy and caring. 
+              <br>
+              A kind face in a crowd of strangers who is always willing to lend a friendly hand to anyone in need.
+              <br>
+               I am motivated to achieve my goals and make my dreams a reality. 
+               <br>
+               My interest in technology began when I watched the Matrix, played Watchdogs on my Xbox, and browsed the internet.
+            </p>
+          <p>Birthday : 11/01/1998 </p>
+          <p>Contact Number : 0618445578</p>
+          <p>Email Address : Swiftyblue11@gmail.com</p>
+          <p>Location : Cape Town</p>
+          <p>Language : English</p>
+          <p>Hobbies : Soccer, Gaming and Music</p>
+          <div id="iconsabout">
+            <i class="bi fa-2x bi-filetype-html"></i>HTML<br>
+            <i class="bi fa-2x bi-filetype-css"></i>CSS<br>
+            <i class="bi fa-2x bi-bootstrap"></i>BOOTSTRAP<br>
+          </div>
+              <button id="about-link" target="_blank" href="https://github.com/Tempestfury11" class="button button2">GitHub</button>
+          </div>
+        </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.about{
+  background-color: #00A5FF;
+  padding: 50px 0 50px 0;
+}
+
+#abouttext{
+  padding: 30px 0 0 0;
+}
+
+.img-fluid {
+  border-radius: 2px;
+  border: 3px solid white;
+}
+
+.button2 {
+  border: none;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button2 {
+  background-color: navy; 
+  color: white; 
+  border: 4px solid #03a9f4;
+}
+
+.button2:hover {
+  background-color: red;
+  color: white;
+}
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,98 @@
+<template>
+  <body>
+      <div class="contact">
+      <form action="https://formspree.io/f/mvoleqaw"
+  method="POST">
+        <div>
+        <img src="https://cdn4.iconfinder.com/data/icons/basic-user-interface-elements/700/mail-letter-offer-256.png" alt="icon">
+         </div>
+        <input type="text" placeholder="Name" name="name" required>
+        <input type="text" placeholder="Email address (required)" required>
+        <input type="text" placeholder="Topic (required)" required>
+        <textarea placeholder="Subject (required)" required></textarea>
+        <input type="submit" value="SEND">
+      </form>
+    </div>
+  </body>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: Helvetica;
+  }
+  
+  body {
+    background-color: #03a9f4;
+    height:97vh;
+    display: flex;
+    justify-content: center;
+    align-items:center;
+  }
+  
+  form {
+    padding-left:25px;
+    padding-right:25px;
+    padding-top:60px;
+    margin: 12px auto;
+    border: 2px solid white;
+    border-radius: 20px;
+    position:relative;
+    z-index:1;
+  }
+  
+  form > div{
+    position:absolute;
+    top:0;
+    left:50%;
+    padding:0 20px;
+    transform:translate(-50%,-50%);
+    background-color: #03a9f4;
+    border: 2px solid white;
+    border-radius: 20px;
+    text-align:center;
+  }
+  
+  form > div > img{
+    filter:invert(1);
+    width:100px;
+  }
+  
+  form > input,
+  form > textarea {
+    padding: 12px;
+    margin: 12px auto;
+    border: 1px solid black;
+    color: #00A5FF;
+    background-color: white;
+    border-radius: 4px;
+    display: block;
+    width: 70vw;
+  }
+  
+  form > textarea{
+    height:100px;
+    resize:none;
+  }
+  
+  form > input[type="submit"] {
+    max-width: 20vw;
+    padding-left: 1%;
+    padding-right: 1%;
+    cursor: pointer;
+    transition: 0.35s;
+  }
+  
+  form > input[type="submit"]:hover {
+    background-color: #00A5FF;
+    color: white;
+  }
+</style>
