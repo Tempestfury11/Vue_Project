514b4ca263c5cb6a64c39f801a26cd62cc160a8b
Tempestfury11 <Swiftyblue11@gmail.com>
2022-07-22 09:36:49 +0200

    If first you dont succeed try again

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Navbar from './components/Navbar.vue'
 
 
 createApp(App).use(store).use(router).mount('#app')
