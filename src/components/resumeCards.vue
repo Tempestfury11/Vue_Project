c6f9af4a50d3063d3e9bde01d56d52ccf0ef1ba1
Tempestfury11 <Swiftyblue11@gmail.com>
2022-09-12 14:28:32 +0200

    resume section done

--- src/components/resumeCards.vue
+++ src/components/resumeCards.vue
@@ -1,32 +1,22 @@
 <template>
-      <div class="container">
-      <div class="row">
-        <div
-          class="col-md-4 col-lg-4 col-12"
-          v-for="item in ResumeArray"
-          :key="item.id"
-        >
-          <section class="card-content">
-            <div class="card-content">
-              <div class="card">
-                <div class="layer"></div>
-                <div class="content">
-                  <br />
-                  <div class="details">
-                    <h2>{{ item.title }}</h2>
-                    <br />
-                    <h4>{{item.duties}}</h4>
-                    <h4>{{item.duration}}</h4>
-                    <h4>{{item.location}}</h4>
-                    <h4>{{item.reference}}</h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
+  <div class="container-fluid">
+    <div class="row">
+      <div
+        class="col-sm-12 col-md-6 col-lg-6 col-12 d-flex justify-content-center mb-3"
+        v-for="item in ResumeArray"
+        :key="item.id"
+      >
+        <div class="card">
+          <p>{{ item.title }}</p>
+          <p>{{ item.duties }}</p>
+          <p>{{ item.duration }}</p>
+          <p>{{ item.location }}</p>
+          <p>{{ item.referances }}</p>
         </div>
+        <br />
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -37,7 +27,8 @@ export default {
         {
           id: 1,
           title: "BES UTILITIES",
-          duties: "Managing customer queries and accounts providing customer service through voice and emails",
+          duties:
+            "Managing customer queries and accounts providing customer service through voice and emails",
           duration: "June 2021 to April 2022",
           location: "Bellville",
           referances: "Keenan Stuurman (071 857 0531)",
@@ -45,7 +36,8 @@ export default {
         {
           id: 2,
           title: "TAKEALOT",
-          duties: "Managing customer queries and accounts providing customer service through voice and emails",
+          duties:
+            "Managing customer queries and accounts providing customer service through voice and emails",
           duration: "June 2020 to August 2020",
           location: "Work From Home",
           referances: "Kirsten Klein (062 390 8944)",
@@ -53,7 +45,8 @@ export default {
         {
           id: 3,
           title: "WOOLWORTHS",
-          duties: "Manage stock control, providing customer service and facilitate transactions through sales",
+          duties:
+            "Manage stock control, providing customer service and facilitate transactions through sales",
           duration: "November 2018 to January 2019",
           location: "Kroombrom, Athlone",
           referances: "Sharon Matthysen (068 257 8605)",
@@ -61,7 +54,8 @@ export default {
         {
           id: 4,
           title: "CAPITA",
-          duties: "Managing customer queries and accounts providing customer service through voice and emails",
+          duties:
+            "Managing customer queries and accounts providing customer service through voice and emails",
           duration: "January 2021 to May 2021",
           location: "Cape Town",
           referances: "Savanna Ensil (063 155 3915)",
@@ -69,42 +63,20 @@ export default {
         {
           id: 5,
           title: "MERCHANTS",
-          duties: "Manage Inbound and outbound calls, providing customer service through emails, LiveChat and Social Media",
+          duties:
+            "Manage Inbound and outbound calls, providing customer service through emails, LiveChat and Social Media",
           duration: "January 2019 to June 2020",
           location: "The District Building, Woodstock",
           referances: "Ralton Pillay(084 066 7197)",
         },
         {
           id: 6,
-          title: "BES UTILITIES",
-          duties: "Managing customer queries and accounts providing customer service through voice and emails",
-          duration: "June 2021 to April 2022",
-          location: "Bellville",
-          referances: "Keenan Stuurman (071 857 0531)",
-        },
-        {
-          id: 7,
-          title: "BES UTILITIES",
-          duties: "Managing customer queries and accounts providing customer service through voice and emails",
-          duration: "June 2021 to April 2022",
-          location: "Bellville",
-          referances: "Keenan Stuurman (071 857 0531)",
-        },
-        {
-          id: 8,
-          title: "BES UTILITIES",
-          duties: "Managing customer queries and accounts providing customer service through voice and emails",
-          duration: "June 2021 to April 2022",
-          location: "Bellville",
-          referances: "Keenan Stuurman (071 857 0531)",
-        },
-        {
-          id: 9,
-          title: "BES UTILITIES",
-          duties: "Managing customer queries and accounts providing customer service through voice and emails",
-          duration: "June 2021 to April 2022",
-          location: "Bellville",
-          referances: "Keenan Stuurman (071 857 0531)",
+          title: "WOOLWORTHS",
+          duties:
+            "Manage stock control, providing customer service and facilitate transactions through sales",
+          duration: "June 2017 to January 2018",
+          location: "Milner Road, Rhondebosch",
+          referances: "Nuraan Samsodien (065 943 5988)",
         },
       ],
     };
@@ -114,16 +86,16 @@ export default {
 
 <style>
 .card {
- width: 190px;
- height: 254px;
- background: rgb(255, 255, 255);
- border-radius: 0.4em;
- box-shadow: 0.3em 0.3em 0.7em #00000015;
- transition: border 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
- border: rgb(250, 250, 250) 0.2em solid;
+  width: 500px;
+  height: 300px;
+  background: black;
+  border-radius: 0.4em;
+  box-shadow: 0.3em 0.3em 0.7em #00000015;
+  transition: border 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  border: rgb(250, 250, 250) 0.2em solid;
 }
 
 .card:hover {
- border: #00a5ff 0.2em solid;
+  border: #00a5ff 0.2em solid;
 }
 </style>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -14,70 +14,8 @@
             </div>
           </div>
           </div>
-          <div id="jobs" class="col-md-6" v-for="item in ResumeArray"
-          :key="item.id">
-            <div class="cards">
-              <p>
-                <h2>{{item.title}}</h2>
-                <h4>Duties</h4>{{item.duties}}
-                <h4>Duration</h4>{{item.duration}}
-                <h4>Location</h4>{{item.location}}
-                <h4>References</h4>{{item.reference}}
-              </p>
-            </div>
-            <br>
-              <div class="cards">
-                <p>
-                  <h2>{TAKEALOT}</h2>
-                  <h4>Duties</h4>Managing customer queries and accounts providing customer service through voice and emails
-                  <h4>Duration</h4>June 2020 to August 2020
-                  <h4>Location</h4>Work From Home
-                  <h4>References</h4>Kirsten Klein (062 390 8944)
-                </p>
-              </div>
-              <br>
-            <div class="cards">
-              <p>
-              <h2>WOOLWORTHS</h2>
-              <h4>Duties</h4>
-              <h4>Duration</h4>
-              <h4>Location</h4>
-              <h4>References</h4>
-              </p>
-            </div>
-          </div>
-          <br>
-          <div id="jobs" class="col-md-6">
-            <div class="cards">
-              <p>
-                <h2>CAPITA</h2>
-                <h4>Duties</h4>
-                <h4>Duration</h4>
-                <h4>Location</h4>
-                <h4>References</h4>
-              </p>
-            </div>
-            <br>
-            <div class="cards">
-              <p>
-                <h2></h2>
-                <h4>Duties</h4>
-                <h4>Duration</h4>
-                <h4>Location</h4>
-                <h4>References</h4>
-              </p>
-            </div>
-            <br>
-            <div class="cards">
-              <p>
-                <h2>WOOLWORTHS</h2>
-                <h4>Duties</h4> Manage stock control, providing customer service and facilitate transactions through sales.
-                <h4>Duration</h4>June 2017 to January 2018
-                <h4>Location</h4>Milner Road, Rhondebosch
-                <h4>References</h4>Nuraan Samsodien (065 943 5988)
-              </p>
-            </div>
-            <br>
+          <div id="jobs">
+            <resumeCards/>
           </div>
         </div>
       </div>
@@ -85,85 +23,9 @@
 </template>
 
 <script>
+import resumeCards from '../components/resumeCards.vue';
 export default {
-  data() {
-    return {
-      ResumeArray: [
-        {
-          id: 1,
-          title: "BES UTILITIES",
-          duties: "Managing customer queries and accounts providing customer service through voice and emails",
-          duration: "June 2021 to April 2022",
-          location: "Bellville",
-          referances: "Keenan Stuurman (071 857 0531)",
-        },
-        {
-          id: 2,
-          title: "https://i.postimg.cc/zfZjP2cx/areaof-Calculater.png",
-          duties: "Area Of Triangle",
-          duration: "https://github.com/Tempestfury11/AreaOfTriangle",
-          location: "https://famous-melba-ddc9b4.netlify.app/",
-          referances:"",
-        },
-        {
-          id: 3,
-          title: "https://i.postimg.cc/05MfGJZk/bmi-Calculator.png",
-          duties: "BMI Calculator",
-          duration: "https://github.com/Tempestfury11/BMI",
-          location: "https://chipper-squirrel-a3a947.netlify.app/",
-          referances:"",
-        },
-        {
-          id: 4,
-          title: "https://i.postimg.cc/8c5xwkK8/calculater.png",
-          duties: "Calculator",
-          duration: "https://github.com/Tempestfury11/Calculator",
-          location: "https://beamish-concha-e3dc78.netlify.app/",
-          referances:"",
-        },
-        {
-          id: 5,
-          title: "https://i.postimg.cc/kg7m0ChH/trojan.png",
-          duties: "Mark IV",
-          duration: "https://github.com/Tempestfury11/TrojanHorse",
-          location: "https://heartfelt-palmier-4a80fa.netlify.app/",
-          referances:"",
-        },
-        {
-          id: 6,
-          title: "https://i.postimg.cc/dtypVtx1/kitarmy.png",
-          duties: "Kit Army",
-          duration: "https://github.com/Reagan701/FullstackProject",
-          location: "https://kitarmy.netlify.app/",
-          referances:"",
-        },
-        {
-          id: 7,
-          title: "https://i.postimg.cc/RhpmxMXm/tempestgaming.png",
-          duties: "Tempest Gaming",
-          duration: "https://github.com/Tempestfury11/CapstoneFrontEnd",
-          location: "https://tempestgaming-2b191.web.app/",
-          referances:"",
-        },
-        {
-          id: 8,
-          title: "https://i.postimg.cc/jdsPCwWd/todoApp.png",
-          duties: "To Do App",
-          duration: "https://github.com/Tempestfury11/todoApp",
-          location: "https://symphonious-cheesecake-7f5d05.netlify.app/",
-          referances:"",
-        },
-        {
-          id: 9,
-          title: "https://i.postimg.cc/nz8M8N1f/marvel.png",
-          duties: "Vue Group Project",
-          duration: "https://github.com/Emivw/Marvel-Inc-vuex",
-          location: "https://62da7ce9b3e78d009c9c448d--luxury-phoenix-954cac.netlify.app/",
-          referances:"",
-        },
-      ],
-    };
-  },
+    components: { resumeCards }
 };
 </script>
 
@@ -186,12 +48,6 @@ export default {
 #heading{
   color: #00a5ff;
 }
-#jobs{
-  padding: 30px 0 30px 0;
-  text-align:center;
-  justify-content: center;
-  align-items: center;
-}
 .cards{
   margin: 0 20px;
   align-items: center;
@@ -200,10 +56,12 @@ export default {
   border: 2px solid #00a5ff;
   border-radius: 20px;
 }
-ul{
-  list-style: none;
-}
 h2{
   color:#00a5ff;
+}
+#jobs{
+    text-align: center;
+    align-items: center;
+    justify-content: center;
 }
 </style>
