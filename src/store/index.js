daa9c0e6b40485a927469061e52d8b09f20fef17
Tempestfury11 <Swiftyblue11@gmail.com>
2022-09-13 14:45:45 +0200

    testimonials added

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,59 @@ export default createStore({
       {
         "name" : "Ashwin Dean",
         "email" : "Ashwin.dean21@gmail.com",
-        "type" : "Peer"
-      }
+        "type" : "Peer",
+        "text" : "I would like to formally recommend Marshalino Jankowski for his consistent work. He has the ability to complete tasks timelessly and effectively. His planning before taking action is always consistent and sharing ideas, steps for planning a website. I know that Marshalino Jankowski has the ability and skills to be an excellent addition to the company",
+        "img" : "https://i.postimg.cc/63vJSqXJ/Ashwin.jpg"
+      },
+      {
+        "name" : "Emile Van Wyk",
+        "email" : "Emiya.vanwyk@gmail.com",
+        "type" : "Peer",
+        "text" : "Marshalino Jankowski, a talented and diligent individual with a can do attitude, in stressful situations he jumps to action and takes charge. In my time I sepnt with him I've seen him put in the effort to improve himself and his work which any team could appreciate. He is an well put together guy easy going and fun to be around but who can  take initiative when needed and lead himself or his team to greatness",
+        "img" : "https://i.postimg.cc/htLxqjhq/Emile2.jpg"
+      },
+      {
+        "name" : "Raees Benny",
+        "email" : "Raeespbenny@gmail.com",
+        "type" : "Peer",
+        "text" : "Marshalino Jankowski is a great person to be around. He always lifts the mood which puts me in a better working mood thus increasing my work ethic and productivity. He is a team player and a natural born leader doing what must be done for the best of the team. He is extremely hard working. I can say Marshalino an amazing person and will be an asset to any company or workplace that he is apart of",
+        "img" : "https://i.postimg.cc/13XwVCNt/Raees-3.jpg"
+      },
+      {
+        "name" : "Monique Dampies",
+        "email" : "Moniqued1861@gmail.com",
+        "type" : "Peer",
+        "text" : "Marshalino Jankowski is one very committed individual and would be a benefit and asset to any team or any work environment that he is a part of. He can be a very cheerful and hardworking individual that codes at 2am experimenting with various code. He does not allow any circumstances to disadvantage him from making a name for himself and being the best that he can be in every situation.",
+        "img" : "https://i.postimg.cc/6pfm8R9p/Monique.jpg"
+      },
+      {
+        "name" : "Saleem Ariefdien",
+        "email" : "Saleemariefdien@gmail.com",
+        "type" : "Peer",
+        "text" : "I was in a class with Marshalino Jankowski for a while and his code writing and research skills are truly phenomenal he brings a positive energy to all group activities and is always willing to help his peers when he can. He always excels at any work that he is given and has excellent time management skills he will be a valuable asset to any team or work environment that he is part of",
+        "img" : "https://i.postimg.cc/CxSvp1jT/Saleem.jpg"
+      },
+      {
+        "name" : "Taucia Jackson",
+        "email" : "Tauciajackson@gmail.com ",
+        "type" : "Peer",
+        "text" : "Marshalino Jankowski is a hardworking and dedicated person with a can-do attitude. In high-pressure circumstances, he leaps into action and assumes control. I have observed him working hard to advance both himself and his work, which is something that any team would value. He is well-dressed, approachable, and entertaining but also capable of taking charge when necessary",
+        "img" : "https://i.postimg.cc/QMJm8m4S/taucia-png-2.jpg"
+      },
+      {
+        "name" : "Cassidy Manual",
+        "email" : "Cassidymanuel@gmail.com",
+        "type" : "Peer",
+        "text" : "I think that Marshalino Jankowski is one of the most hardworking people i know, he is one of those people that was blessed with a never give up attitude. He is a great asset to the team as he has an infectious smile and he always makes the people around him laugh with making light out of stressful situations. When work needs to be done you can find him online at early hours pushing his code",
+        "img" : "https://i.postimg.cc/yx2507Wz/Cassidy-1.jpg"
+      },
+      {
+        "name" : "Godwin Dzvapatsva",
+        "email" : "godwin@lifechoices.co.za",
+        "type" : "Lecturer",
+        "text" : "Marshalino is a person with value, stature and very good manners. He presents a true gentleman position and shows a lot of integrity. He is a hardworker, tries his very best in every task given to him and is able to complete it before the actual date or time due. May Marshalino Jankowski be given a chance to this experience because I assure you he will be eager to learn.",
+        "img" : "https://i.postimg.cc/NfbM1Mcc/15.jpg"
+      },
     ]
   },
   getters: {
