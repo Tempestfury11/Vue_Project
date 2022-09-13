f55bf450f2c4a06dcb8962225ad31ce667930847
Tempestfury11 <Swiftyblue11@gmail.com>
2022-09-13 15:50:42 +0200

    jobs text styled

--- src/components/resumeCards.vue
+++ src/components/resumeCards.vue
@@ -27,8 +27,7 @@ export default {
         {
           id: 1,
           title: "BES UTILITIES",
-          duties:
-            "Managing customer queries and accounts providing customer service through voice and emails",
+          duties: "Managing customer queries and accounts providing customer service through voice and emails",
           duration: "June 2021 to April 2022",
           location: "Bellville",
           referances: "Keenan Stuurman (071 857 0531)",
@@ -36,8 +35,7 @@ export default {
         {
           id: 2,
           title: "TAKEALOT",
-          duties:
-            "Managing customer queries and accounts providing customer service through voice and emails",
+          duties: "Managing customer queries and accounts providing customer service through voice and emails",
           duration: "June 2020 to August 2020",
           location: "Work From Home",
           referances: "Kirsten Klein (062 390 8944)",
@@ -45,8 +43,7 @@ export default {
         {
           id: 3,
           title: "WOOLWORTHS",
-          duties:
-            "Manage stock control, providing customer service and facilitate transactions through sales",
+          duties: "Manage stock control, providing customer service and facilitate transactions through sales",
           duration: "November 2018 to January 2019",
           location: "Kroombrom, Athlone",
           referances: "Sharon Matthysen (068 257 8605)",
@@ -54,8 +51,7 @@ export default {
         {
           id: 4,
           title: "CAPITA",
-          duties:
-            "Managing customer queries and accounts providing customer service through voice and emails",
+          duties: "Managing customer related queries and accounts providing customer service through voice and emails",
           duration: "January 2021 to May 2021",
           location: "Cape Town",
           referances: "Savanna Ensil (063 155 3915)",
@@ -63,8 +59,7 @@ export default {
         {
           id: 5,
           title: "MERCHANTS",
-          duties:
-            "Manage Inbound and outbound calls, providing customer service through emails, LiveChat and Social Media",
+          duties: "Manage Inbound and outbound calls, providing customer service through emails, LiveChat and Social Media",
           duration: "January 2019 to June 2020",
           location: "The District Building, Woodstock",
           referances: "Ralton Pillay(084 066 7197)",
@@ -72,8 +67,7 @@ export default {
         {
           id: 6,
           title: "WOOLWORTHS",
-          duties:
-            "Manage stock control, providing customer service and facilitate transactions through sales",
+          duties: "Manage stock control, providing customer service and facilitate transactions through sales",
           duration: "June 2017 to January 2018",
           location: "Milner Road, Rhondebosch",
           referances: "Nuraan Samsodien (065 943 5988)",
@@ -87,7 +81,7 @@ export default {
 <style scoped>
 .card {
   width: 500px;
-  height: 300px;
+  height: 530px;
   background: black;
   border-radius: 0.4em;
   box-shadow: 0.3em 0.3em 0.7em #00000015;
